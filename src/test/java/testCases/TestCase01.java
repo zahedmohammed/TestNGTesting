94394f253b7f4fafa62d2a656b4ae52f86d5f861
Mohammed Zahed <zahed.mohammed@gmail.com>
2020-07-19 10:56:55 +0530

    Added Test01 with Methods

--- src/test/java/testCases/TestCase01.java
+++ src/test/java/testCases/TestCase01.java
@@ -1,5 +1,41 @@
 package testCases;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class TestCase01 {
+	
+	@BeforeTest
+	public static void openDbConnection() {
+		System.out.println("DB Connection Established");
+	}
+	
+	@AfterTest
+	public static void closeDbConnection() {
+		System.out.println("DB Connection Terminated");
+	}
+	
+	@BeforeMethod
+	public static void launchBrowser() {
+		System.out.println("Launching Browser");
+	}
+	
+	@AfterMethod
+	public static void closeBrowser() {
+		System.out.println("Closing Browser");
+	}
+	
+	@Test
+	public static void login() {
+		System.out.println("Login successfully");
+	}
+	
+	@Test(priority=1)
+	public static void userRegistration() {
+		System.out.println("Registration Successful");
+	}
 
 }
